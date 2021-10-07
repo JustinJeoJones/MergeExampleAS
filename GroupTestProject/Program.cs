731e345d77e22a7db738ee611d73c91d98c2d5cb
Jeo <jeo961@gmail.com>
2021-10-07 11:03:52 -0400

    first commit

---
+++ GroupTestProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GroupTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static int adder(int num1, int num2)
+        {
+            return 0;
+        }
+
+        static int subber(int num1, int num2)
+        {
+            return 0;
+        }
+    }
+}
